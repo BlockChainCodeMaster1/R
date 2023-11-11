193e4e46dc942a1d8f885e365c69ae7a6397fd8a
aemoe <>
2023-11-12 03:06:39 +0800

    fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>REVS - Everyone is revolutionaries.</title>
+        <meta property="og:title" content="REVS" key="title" />
+        <meta name="description" content="Everyone is revolutionaries." key="desc" />
+      </Head>
       <body className=' bg-black'>
         <Main />
         <NextScript />
